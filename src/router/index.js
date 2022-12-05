bbe72ad0e3ab74470b8d64e6f0a5edfa4b842bb5
Charleen Konsa <charleen.konsa.232@gmail.com>
2022-12-05 22:44:08 +0200

    Creating files and paths for HW4

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,39 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 import SignupView from '@/views/SignupView.vue'
+import LogInView from '@/views/LogInView.vue'
+import ContactView from '@/views/ContactView.vue'
+import auth from "../auth"
 
 const routes = [{
         path: '/',
         name: 'MainView',
         component: MainView,
-        alias:'/main'
+        alias:'/main',
+        beforeEnter: async(to, from, next) => {
+            let authResult = await auth.authenticated();
+            if (!authResult) {
+                next('/login')
+            } else {
+                next();
+            }
+        }
     },
     {
         path: '/signup',
         name: 'SignupView',
         component: SignupView
-    }
+    },
+    {
+        path: "/login",
+        name: "LogInView",
+        component: LogInView,
+    },
+    {
+        path: "/contact",
+        name: "ContactView",
+        component: ContactView,
+    },
 ]
 
 const router = createRouter({
